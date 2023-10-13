7b8990ff99cdbc61fbc671a1f98a151107e77eca
MikPoik <42643139+MikPoik@users.noreply.github.com>
2023-10-13 19:08:00 +0000

    Initial commit

---
+++ example_client.py
@@ -0,0 +1,25 @@
+from steamship import Block, File, MimeTypes, Steamship, Task
+from time import sleep
+
+
+def main():
+  client = Steamship(workspace="coqui-dev-ws")
+
+  generator = client.use_plugin("coquidev", config={"coqui_api_key": ""})
+
+  text = "Want to hear a song?"
+  task = generator.generate(
+      text=text,
+      append_output_to_file=True,
+      make_output_public=True,
+  )
+
+  task.wait()
+  url = f"https://api.steamship.com/api/v1/block/{task.output.blocks[0].id}/raw"
+
+  print(url)
+  return task.output.blocks
+
+
+if __name__ == "__main__":
+  main()
---
+++ test/coquitest.py
@@ -0,0 +1,20 @@
+import requests
+
+url = 'https://app.coqui.ai/api/v2/samples/xtts/render/'
+headers = {'Authorization': 'Bearer ', 'accept': 'audio/wav'}
+
+data = {
+    "voice_id": "c791b5b5-0558-42b8-bb0b-602ac5efc0b9",
+    "text": "Hello",
+    "language": "en"
+}
+
+response = requests.post(url, headers=headers, json=data)
+
+# Check the response
+if response.status_code == 200:
+  print("Request was successful")
+  print(response.json())
+else:
+  print("Request failed with status code:", response.status_code)
+  print(response.text)
